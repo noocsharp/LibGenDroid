fd4329ce6c531e7e1af8965c3236a27b933df302
noocsharp <noocsharp@gmail.com>
2019-03-03 21:44:45 -0600

    initial commit

---
+++ app/src/main/java/com/noocsharp/libgendroid/BookEntry.java
@@ -0,0 +1,89 @@
+package com.noocsharp.libgendroid;
+
+public class BookEntry {
+    String author, title, publisher, language, extension, size;
+    int id, pages, year;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String toString() {
+        return "id: " + id +
+            ", author: " + author +
+            ", title: " + title +
+            ", publisher: " + publisher +
+            ", language: " + language +
+            ", extension: " + extension +
+            ", pages: " + pages +
+            ", size: " + size;
+    }
+}
---
+++ app/src/main/java/com/noocsharp/libgendroid/MainActivity.java
@@ -0,0 +1,93 @@
+package com.noocsharp.libgendroid;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    //EditText edittext = findViewById(R.id.editText);
+    ArrayList<BookEntry> entries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new HandleSearchTask().execute("devil");
+    }
+
+    private class HandleSearchTask extends AsyncTask<String, Void, ArrayList<BookEntry>> {
+        @Override
+        protected ArrayList<BookEntry> doInBackground(String... strings) {
+            ArrayList<BookEntry> bookList = new ArrayList<>();
+
+            if (strings.length <= 0) return null;
+            try {
+                Document doc = Jsoup.connect("http://gen.lib.rus.ec/search.php?req=" + strings[0] + "&open=0&res=25&view=simple&phrase=1&column=def" + strings[0]).get();
+                Elements rows = doc.getElementsByClass("c").first().children().first().children();
+
+                boolean first = true;
+                for (Element row : rows) {
+                    if (first) {
+                        first = false;
+                        continue;
+                    }
+                    Elements elements = row.children();
+
+                    BookEntry entry = new BookEntry();
+
+                    entry.setId(Integer.parseInt(elements.get(0).text()));
+                    entry.setAuthor(elements.get(1).text());
+                    entry.setTitle(parseTitle(elements.get(2).children().last().html()));
+                    entry.setPublisher(elements.get(3).text());
+                    entry.setYear(Integer.parseInt(elements.get(4).text()));
+                    entry.setPages(parsePages(elements.get(5).text()));
+                    entry.setLanguage(elements.get(6).text());
+                    entry.setSize(elements.get(7).text());
+                    entry.setExtension(elements.get(8).text());
+
+                    Log.i(TAG, "ROW");
+                    Log.i(TAG, entry.toString());
+                    Log.i(TAG, row.html());
+
+                    bookList.add(entry);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bookList;
+        }
+
+        private String parseTitle(String title) {
+            if (title.indexOf('<') != -1)
+                return title.substring(0, title.indexOf('<'));
+            else
+                return title;
+        }
+
+        private int parsePages(String pages) {
+            if (pages.indexOf(' ') != -1)
+                return Integer.parseInt(pages.substring(0, pages.indexOf(' ')));
+            else
+                return Integer.parseInt(pages);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<BookEntry> bookEntries) {
+
+        }
+    }
+}
